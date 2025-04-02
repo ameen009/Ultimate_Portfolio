b9754ba266d658758c4511150e1ca8ca4dd14be4
Ameen Bari <ameen.bari@sap.com>
2025-04-03 01:20:20 +0530

    New Changes

--- next-gen-portfolio/src/components/topics/url-card/url-card.component.ts
+++ next-gen-portfolio/src/components/topics/url-card/url-card.component.ts
@@ -3,11 +3,12 @@ import { CardModule } from 'primeng/card';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { DialogModule } from 'primeng/dialog';
 import { UrlShortnerComponent } from './url-shortner/url-shortner.component';
+import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-url-card',
   standalone: true,
-  imports: [UrlShortnerComponent, CardModule, OverlayPanelModule, DialogModule],
+  imports: [UrlShortnerComponent, CardModule, OverlayPanelModule, DialogModule, Button],
   templateUrl: './url-card.component.html',
   styleUrl: './url-card.component.css'
 })
